9608ec9a90d8befd0e32af639e178663c2b3ace0
TheliSaikiran <126541078+TheliSaikiran@users.noreply.github.com>
2023-12-07 16:20:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function bar() {
+    var x=document.getElementById('phn');
+    x.classList.toggle("display-mobile");
+
+}
+// document.querySelectorAll(".nav-link").forEach(n=> n.addeventlistener("click",()=>{
+//     hamburger.classList.remove("active");
+//     navmenu.classList.remove("active");
+
+// }))
+
